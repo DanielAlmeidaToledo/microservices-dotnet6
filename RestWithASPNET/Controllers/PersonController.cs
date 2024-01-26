6867001137caa036aa644afd7d05996ec31fe816
Daniel Toledo <dan992965@gmail.com>
2024-01-26 01:41:07 -0300

    feat: adiciona endpoints na controller

--- RestWithASPNET/Controllers/PersonController.cs
+++ RestWithASPNET/Controllers/PersonController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using RestWithASPNET.Model;
+using RestWithASPNET.Services;
 
 namespace RestWithASPNET.Controllers
 {
@@ -8,17 +10,47 @@ namespace RestWithASPNET.Controllers
     {
 
         private readonly ILogger<PersonController> _logger;
+        private IPersonService _personService;
 
-        public PersonController(ILogger<PersonController> logger)
+        public PersonController(ILogger<PersonController> logger, IPersonService personService)
         {
             _logger = logger;
+            _personService = personService;
         }
 
-        [HttpGet("{oper}/{firstNumber}/{secondNumber}")]
-        public IActionResult Get(string oper, string firstNumber, string secondNumber)
+        [HttpGet]
+        public IActionResult Get()
         {
-
-            return BadRequest("Invalid Input");
+            return Ok(_personService.FindAll());
+        }       
+        
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            var person = _personService.FindByID(id);
+            if(person == null) return NotFound();
+            return Ok(person);
+        }      
+        
+        [HttpPost]
+        public IActionResult Post([FromBody] Person person)
+        {
+            if(person == null) return BadRequest();
+            return Ok(_personService.Create(person));
+        }       
+        
+        [HttpPut]
+        public IActionResult Put([FromBody] Person person)
+        {
+            if(person == null) return BadRequest();
+            return Ok(_personService.Update(person));
+        }      
+        
+        [HttpDelete]
+        public IActionResult Delete(long id)
+        {
+            _personService.Delete(id);
+            return NoContent();
         }
     }
 }
